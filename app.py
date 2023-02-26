72a1f5b18f4ce85abb141f3afd30b377b03405b1
Mateus Bastos <105644793+mateusbastosdeoliveira@users.noreply.github.com>
2023-02-26 18:00:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,21 @@ while validacao_senha == False:
         check_credenciais = False
         while check_credenciais == False:
             check_nome = str(input("Digite o nome cadastrado : "))
-            check_senha = str(input("Digite a senha cadastrada : "))
-            if check_nome == cadastro_nome and check_senha == cadastro_senha:
-                check_credenciais = True
-                print("Acesso concedido!")
+           
+            if check_nome == cadastro_nome:
+                check_senha = str(input("Digite a senha cadastrada : "))
+                if check_senha == cadastro_senha:
+                    check_credenciais = True
+                    print("Acesso concedido")
+                else:
+                    check_credenciais = False
+                    print("Senha incorreta, tente novamente")
+                    
+                
             else:
-                print("Nome ou senha incorretos, tente novamente")
                 check_credenciais = False
+                print("Nome incorreto, tente novamente")
+                
 
     else:
         print("Senha deve conter mais que 5 caracteres,tente novamente")
